86cb83b5559543d82e796fab337efe50fbc8de90
pavanraj8943 <pavan.64j@gmail.com>
2025-10-24 17:58:54 +0530

    adding

---
+++ dom.js
@@ -0,0 +1,50 @@
+// document.getElementsByTagName("input")[0].value="hii"
+// document.getElementsByTagName("input")[0].value
+// document.getElementsByClassName("name").value
+// document.getElementById("email").value
+// document.querySelector(".name")
+// document.querySelectorAll("input")
+
+function handleClick(){
+    alert("logitech")
+}
+
+// function handlekey(){
+
+//    let x=document.getElementById("name").value
+//    console.log(x);
+   
+// }
+
+// paramiter pass
+
+
+// function handlekey(e){
+//     console.log(e.value);
+    
+// }
+
+
+
+// function handleadd(){
+//     let a=parseInt(document.getElementById("fno").value)
+//     let b=parseInt(document.getElementById("sno").value)
+//     let c = a+b;
+//     document.getElementById("res").textContent=c                //out put display akum
+//     // document.getElementById("res").innerHTML='<p>hai</p>'    //just text condent display akum
+// }
+
+
+// document.getElementById("new").addEventListener('click',()=>{
+//     let x=document.getElementById("num").value
+//     str=''
+//     for(i=0;i<=x;i++){
+//        
+
+//     }
+//     document.getElementById("res").innerHTML=str
+// })
+
+
+
+
